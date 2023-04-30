ae1a9ccb260a0b0303a3fecabeb3b2b63878b024
SvenRehm <rehm21@web.de>
2023-04-30 02:54:01 +0200

    supabase

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,33 @@
 import Counter from "~/components/Counter";
+import { Component, createEffect, createSignal } from 'solid-js'
+import { supabase } from '../supabaseClient.js'
+import { AuthSession } from '@supabase/supabase-js'
+import Account from '~/components/Account.jsx'
+import Auth from '~/components/Auth.jsx'
 
 export default function Home() {
-    return (
-        <main class="text-center mx-auto text-gray-700 p-4">
-                <Counter />
-        </main>
-    );
+  const [session, setSession] = createSignal(null)
+
+  createEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  })
+
+  return (
+    <div class="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session() ? <Auth /> : <Account session={session()} />}
+    </div>
+  )
 }
+
+// export default function Home() {
+//     return (
+//         <main class="text-center mx-auto text-gray-700 p-4">
+//                 <Counter />
+//         </main>
+//     ); }
